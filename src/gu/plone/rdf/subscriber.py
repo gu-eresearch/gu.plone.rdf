6a07b6d54be1b005547ed53a90283f17629af4e8
Gerhard Weis <gerhard.weis@gmail.com>
2013-03-25 13:02:19 +1000

    only copy plone fields to rdf if they are not populated already

--- src/gu/plone/rdf/subscriber.py
+++ src/gu/plone/rdf/subscriber.py
@@ -58,11 +58,13 @@ def InitialiseGraph(object, event):
     # FIXME: use only noe way to describ ethings ....
     #        see dc - rdf mapping at http://dublincore.org/documents/dcq-rdf-xml/
     #        maybe dc app profile not as good as it might sound, but translated to RDF is better (or even owl)
-    graph.add((graph.identifier, DCTERMS['title'], Literal(object.title)))
-    graph.add((graph.identifier, RDFS['label'], Literal(object.title)))
-    graph.add((graph.identifier, DCTERMS['description'], Literal(object.description)))
-    graph.add((graph.identifier, RDFS['comment'], Literal(object.description)))
-
+    for prop, val in ((DCTERMS['title'], Literal(object.title)),
+                      (RDFS['label'], Literal(object.title)),
+                      (DCTERMS['description'], Literal(object.description)),
+                      (RDFS['comment'], Literal(object.description)),
+                      ):
+        if not graph.value(graph.identifier, prop):
+            graph.add((graph.identifier, prop, val))
     # FIXME: persist stuff here? (would be nice to make this part of normal transaction)
     handler = getUtility(IORDF).getHandler()
     # ... check for temporary folder...
