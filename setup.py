4d5f14646f8cf72598cc4d93fa8483279aca1980
Gerhard Weis <gerhard.weis@gmail.com>
2013-08-30 14:17:23 +1000

    prepare 2.0 release

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 import os
 
-version = '2.0dev'
+version = '2.0'
 
 setup(name='gu.plone.rdf',
       version=version,
@@ -28,9 +28,9 @@ setup(name='gu.plone.rdf',
           'setuptools',
           'Products.CMFPlone',
           'plone.app.registry',
-          'gu.z3cform.rdf',
+          'gu.z3cform.rdf<0.5dev',
           'ordf',
-          'rdflib-zodb',
+          'rdflib-zodb<1.0dev',
           'collective.z3cform.datetimewidget'
       ],
       extras_require={
