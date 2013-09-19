28fd9c885ffb15c0ee747ca608cd4c1aa875f7bb
Gerhard Weis <gerhard.weis@gmail.com>
2013-09-19 16:19:10 +1000

    moved subformadapter and formextender from gu.plone.rdf  to gu.z3cform.rdf

--- src/gu/plone/rdf/converter.py
+++ src/gu/plone/rdf/converter.py
@@ -1,15 +1,11 @@
 # maybe use own exceptions here?
 from collective.z3cform.datetimewidget.interfaces import DateValidationError
-from gu.z3cform.rdf.converter import BaseDataConverter, RDFObjectSubForm
+from gu.z3cform.rdf.converter import BaseDataConverter
 from rdflib import Literal
 from ordf.namespace import XSD, DC
 from datetime import date
-from plone.app.z3cform.interfaces import IPloneFormLayer
 import zope.interface
 import zope.component
-from z3c.form.interfaces import ISubformFactory
-from gu.z3cform.rdf.interfaces import IRDFObjectField
-from gu.z3cform.rdf.widgets.interfaces import IRDFObjectWidget
 from isodate import parse_date
 
 
@@ -87,32 +83,6 @@ class RDFDateRangeConverter(BaseDataConverter):
         return Literal(unicode(litval), datatype=DC['Period'])
 
 
-class SubformAdapter(object):
-    """Most basic-default subform factory adapter"""
-
-    zope.interface.implements(ISubformFactory)
-    zope.component.adapts(zope.interface.Interface,  # widget value
-                          IPloneFormLayer,  # IFormLayer,    #request
-                          zope.interface.Interface,  # widget context
-                          zope.interface.Interface,  # form
-                          IRDFObjectWidget,  # widget
-                          IRDFObjectField,  # field
-                          zope.interface.Interface)  # field.schema
-    factory = RDFObjectSubForm
-
-    def __init__(self, context, request, widgetContext, form,
-                 widget, field, schema):
-        self.context = context  # context for this form
-        self.request = request  # request
-        self.widgetContext = widgetContext  # main context
-        self.form = form  # main form
-        self.widget = widget  # the widget tha manages this form
-        self.field = field  # the field to attach the whole thing to
-        self.schema = schema  # we don't use this
-
-    def __call__(self):
-        obj = self.factory(self.context, self.request, self.widget)
-        return obj
 
 
 import re
